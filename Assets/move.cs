4ce3d5aa5825ca9127fb2bfe15b2880902053889
Amanda Han <aliyuanh@gmail.com>
2017-02-27 14:09:49 -0800

    Added name, UI, start + end
    Cleaned up the move script, added end and start screens, added some
    textures. Rotated platforms.

--- Assets/move.cs
+++ Assets/move.cs
@@ -16,7 +16,6 @@ public class move : MonoBehaviour {
     Direction direction = Direction.RIGHT;
     Vector3 newVelocity;
 	Vector3 ground;
-
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -26,55 +25,9 @@ public class move : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        newVelocity = rb.velocity;
-
-        if (Input.GetKeyDown (KeyCode.W) && jumpsRemaining > 0) {
-            jumpsRemaining--;
-            newVelocity.y = jumpStrength;
-		}
-
-		if (Input.GetKeyDown (KeyCode.S)) {
-            newVelocity.y -= downJumpForce;
-		}
-
-		if (Input.GetKey (KeyCode.A) && (Mathf.Abs(rb.velocity.x) < 100) ){
-            if(direction != Direction.RIGHT)
-            {
-                newVelocity.x = 0;
-            }
-			rb.AddForce (Vector3.left * walkingSpeed);
-            direction = Direction.RIGHT;
-		}
-
-		if(Input.GetKey(KeyCode.D) && Mathf.Abs(rb.velocity.x) < 100){
-            if (direction != Direction.LEFT)
-            {
-                newVelocity.x = 0;
-            }
-            rb.AddForce(Vector3.right * walkingSpeed);
-            direction = Direction.LEFT;
-		}
-
-		if (Mathf.Abs(rb.velocity.x) > 1f) {
-			
-				an.Play ("Walk");
-		} else {
-			an.Play ("Stand");
-		}
-
-
-        rb.velocity = newVelocity;
-
-        if (direction == Direction.RIGHT){
-		    transform.localEulerAngles = new Vector3(0,-90,0);
-
-	    }else{
-		    transform.localEulerAngles = new Vector3(0,90,0);
-	    }
-
-
-
+		movePlayer ();
+		anim ();
+		swapDir ();
 	}
 	void OnTriggerEnter(Collider col){
 		if(col.gameObject.CompareTag("platform") && transform.position.y > col.gameObject.transform.position.y){
@@ -94,6 +47,58 @@ public class move : MonoBehaviour {
             jumpsRemaining = jumps;
 		}
 
+
+	}
+	public void swapDir(){
+		if (direction == Direction.RIGHT){
+			transform.localEulerAngles = new Vector3(0,-90,0);
+
+		}else{
+			transform.localEulerAngles = new Vector3(0,90,0);
+		}
+
+	}
+
+	public void anim(){
+		if (Mathf.Abs(rb.velocity.x) > 1f) {
+			an.Play ("Walk");
+		} else {
+			an.Play ("Stand");
+		}
+
+	}
+
+	public void movePlayer(){
+		newVelocity = rb.velocity;
+
+		if (Input.GetKeyDown (KeyCode.W) && jumpsRemaining > 0) {
+			jumpsRemaining--;
+			newVelocity.y = jumpStrength;
+		}
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			newVelocity.y -= downJumpForce;
+		}
+
+		if (Input.GetKey (KeyCode.A) && (Mathf.Abs(rb.velocity.x) < 100) ){
+			if(direction != Direction.RIGHT)
+			{
+				newVelocity.x = 0;
+			}
+			rb.AddForce (Vector3.left * walkingSpeed);
+			direction = Direction.RIGHT;
+		}
+
+		if(Input.GetKey(KeyCode.D) && Mathf.Abs(rb.velocity.x) < 100){
+			if (direction != Direction.LEFT)
+			{
+				newVelocity.x = 0;
+			}
+			rb.AddForce(Vector3.right * walkingSpeed);
+			direction = Direction.LEFT;
+		}
+
+		rb.velocity = newVelocity;
 
 	}
 
---
+++ Assets/startScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class startScreen : MonoBehaviour {
+	Button btn;
+	// Use this for initialization
+	void Start () {
+		btn = GetComponent<Button> ();
+		btn.onClick.AddListener (toGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void toGame(){
+		SceneManager.LoadScene ("main");
+
+	}
+}
