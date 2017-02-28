e0e52640a769a01a6eaa90be4fac6c32b2171822
Amanda Han <aliyuanh@gmail.com>
2017-02-28 13:59:19 -0800

    Timer, end cleanup
    Added a timer to the UI (rounded to the nearest second). Cleaned up the
    end thing. Just type in the time for the max time, but I set it as 10
    for ease of use.

---
+++ Assets/timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timer : MonoBehaviour {
+	Text txt ;
+	float time;
+	// Use this for initialization
+	void Start () {
+		txt = GetComponent<Text> ();
+		time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		txt.text = "" + Mathf.Round(time);
+	}
+}
--- Assets/toEnd.cs
+++ Assets/toEnd.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class toEnd : MonoBehaviour {
-
+	public float maxTime;
+	float currentTime;
 	// Use this for initialization
 	void Start () {
-		
+		currentTime = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		currentTime += Time.deltaTime;
+		Debug.Log(currentTime);
+		if(currentTime >= maxTime){
+			SceneManager.LoadScene("lose");
+		}
 	}
 
 	void OnCollisionEnter(Collision col){
